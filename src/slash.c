96a35ef92ef067f331ab1b5a3b8a869c315142f7
diamant <diamant.alexandre@gmail.com>
2022-11-11 12:28:44 +0100

    Ajouts fichiers essentiels

---
+++ src/slash.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#define MAX_ARGS_NUMBER 4096
+#define MAX_ARG_STRLEN 4096
+
+int slash() {
+
+}
+
+int main(int argc, char *argv[]) {
+    slash();
+
+    return EXIT_SUCCESS;
+}
